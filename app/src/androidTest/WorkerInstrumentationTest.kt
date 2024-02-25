98678c6a40ce87229c8e77dd89d15639a58dcbf0
Pedro-Calderon <s18120164@alumnos.itsur.edu.mx>
2024-02-24 22:46:15 -0600

    Se finalizo el codelab con las pruebas para las implementaciones de worker.

---
+++ app/src/androidTest/WorkerInstrumentationTest.kt
@@ -0,0 +1,26 @@
+class WorkerInstrumentationTest {
+    private lateinit var context: Context
+
+    @Before
+    fun setUp() {
+        context = ApplicationProvider.getApplicationContext()
+    }
+
+    @Test
+    fun saveImageToFileWorker_doWork_resultSuccessReturnsUrl() {
+        val worker = TestListenableWorkerBuilder<SaveImageToFileWorker>(context)
+            .setInputData(workDataOf(mockUriInput))
+            .build()
+        runBlocking {
+            val result = worker.doWork()
+            val resultUri = result.outputData.getString(KEY_IMAGE_URI)
+            assertTrue(result is ListenableWorker.Result.Success)
+            assertTrue(result.outputData.keyValueMap.containsKey(KEY_IMAGE_URI))
+            assertTrue(
+                resultUri?.startsWith("content://media/external/images/media/")
+                    ?: false
+            )
+        }
+    }
+}
+KEY_IMAGE_URI to "android.resource://com.example.bluromatic/drawable/android_cupcake"
